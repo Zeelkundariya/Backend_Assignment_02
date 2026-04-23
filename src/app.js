ca4f03d46106488b634418251531ee1f94c4361e
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-04-23 19:33:50 +0530

    feat: initial project setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes Placeholder
+// app.use("/api/notes", require("./routes/note.routes"));
+
+// Basic Health Check
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "Notes API is running" });
+});
+
+module.exports = app;
